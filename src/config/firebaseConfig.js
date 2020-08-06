8e13a272e26467a74c8afcb20c46ef9ea3ca2844
andrewk1999 <andrew.kong@stonybrook.edu>
2020-08-05 20:43:27 -0400

    Modified firebase configuration and name

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
 var firebaseConfig = {
-  apiKey: "AIzaSyBWajLPborfkMPrrcXbKktrzodEROl1h3w",
-  authDomain: "wireframer-project-4ea6c.firebaseapp.com",
-  databaseURL: "https://wireframer-project-4ea6c.firebaseio.com",
-  projectId: "wireframer-project-4ea6c",
-  storageBucket: "wireframer-project-4ea6c.appspot.com",
-  messagingSenderId: "1074105843099",
-  appId: "1:1074105843099:web:f3b1e0661ce2130f9ab53e",
-  measurementId: "G-WWSSM1WYNH"
+  apiKey: "AIzaSyAo4APmc3x_nmfL7WwdgnETR9l_8QdZ8Ok",
+  authDomain: "schedulebuilder-4382d.firebaseapp.com",
+  databaseURL: "https://schedulebuilder-4382d.firebaseio.com",
+  projectId: "schedulebuilder-4382d",
+  storageBucket: "schedulebuilder-4382d.appspot.com",
+  messagingSenderId: "623968118832",
+  appId: "1:623968118832:web:4007445732eaea791a25f5",
+  measurementId: "G-JMHV4RMXY9"
 };
 firebase.initializeApp(firebaseConfig);
 
