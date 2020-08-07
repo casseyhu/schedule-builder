28623552bd771a60aa04b0d0d3624e5c4873596f
casseyhu <casseyhu123@gmail.com>
2020-08-07 19:42:13 -0400

    add basic course search template

--- src/components/home_screen/ScheduleSearch.js
+++ src/components/home_screen/ScheduleSearch.js
@@ -2,10 +2,28 @@ import React from 'react'
 
 const ScheduleSearch = () => {
     return (
-        <div>
-            <p>schedule search</p>
+        <div className='s-search-component'>
+            <h5> Search Course </h5>
+            <p> Search by Subject </p>
+            <select class="browser-default">
+                <option value="" disabled selected>Choose Subject</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+            </select>
+            <select class="browser-default">
+                <option value="" disabled selected>Choose Course Number</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+            </select>
+            <div className="course-description">
+                <div> Description blah blah </div>
+                <button id='add-button' className="btn pink lighten-1 z-depth-0"> Add </button>
+            </div>
         </div>
     )
 }
+
 
 export default ScheduleSearch
