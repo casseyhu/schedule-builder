72febcf7a481558d9c5e5d9efc735e0298656bb3
andrewk1999 <andrew.kong@stonybrook.edu>
2020-08-08 15:13:34 -0400

    Temporary scroll bar for schedule table

--- src/components/home_screen/ScheduleTable.js
+++ src/components/home_screen/ScheduleTable.js
@@ -5,23 +5,25 @@ import { Checkbox } from 'react-materialize';
 const ScheduleTable = ({courses}) => {
     return (
         <div className="course-list">
-            <table className="highlight">
-            <thead>
-                <tr>
-                <th>Checkmark</th>
-                <th>Course</th>
-                <th>Instructor</th>
-                <th>Meeting Time</th>
-                <th>Delete</th>
-                <th></th>
-                </tr>
-            </thead>
-            {courses && courses.map(course => {
-                return (
-                    <CourseSummary course={course} key={course.id} />
-                )
-            })}
-            </table>
+            <div className="schedule-scroll">
+                <table className="highlight">
+                <thead>
+                    <tr>
+                    <th>Checkmark</th>
+                    <th>Course</th>
+                    <th>Instructor</th>
+                    <th>Meeting Time</th>
+                    <th>Delete</th>
+                    <th></th>
+                    </tr>
+                </thead>
+                {courses && courses.map(course => {
+                    return (
+                        <CourseSummary course={course} key={course.id} />
+                    )
+                })}
+                </table>
+            </div>
         </div>
     )
 }
