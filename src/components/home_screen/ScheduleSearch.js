0f8f8c8149116a2e639282e253e98a0661a5978f
casseyhu <casseyhu123@gmail.com>
2020-08-07 21:01:05 -0400

    fix course search layout

--- src/components/home_screen/ScheduleSearch.js
+++ src/components/home_screen/ScheduleSearch.js
@@ -1,29 +1,53 @@
-import React from 'react'
+import React from 'react';
+import firebase from '../../config/firebaseConfig';
 
 const ScheduleSearch = () => {
     return (
         <div className='s-search-component'>
             <h5> Search Course </h5>
             <p> Search by Subject </p>
-            <select class="browser-default">
+            {/* onChange not working */}
+            <select id='subject-drop' class="browser-default" onChange="onSubjChange();">
                 <option value="" disabled selected>Choose Subject</option>
                 <option value="1">Option 1</option>
                 <option value="2">Option 2</option>
                 <option value="3">Option 3</option>
             </select>
-            <select class="browser-default">
+            <select id='number-drop' class="browser-default">
                 <option value="" disabled selected>Choose Course Number</option>
                 <option value="1">Option 1</option>
                 <option value="2">Option 2</option>
                 <option value="3">Option 3</option>
             </select>
-            <div className="course-description">
-                <div> Description blah blah </div>
+            <div id="course-description" class='row'>
+                <div className="col s3 push-s9">
                 <button id='add-button' className="btn pink lighten-1 z-depth-0"> Add </button>
+                </div>
+                <div className="col s9 pull-s3">
+                    Description blah blah blah blah blah blah blah 
+                    blah blah blah blah blah blah blah blah blah blah 
+                    blah blah blah blah
+                </div>
             </div>
         </div>
     )
 }
 
+
+function onSubjChange() {
+    console.log("NOT  HERE ? getting course subj");
+    var d = document.getElementById('subject-drop');
+    var opt = null;
+    // Get subject list from firestore
+    const snapshot = firebase.firestore().collection('courses').get();
+    var courses = snapshot.docs.map(doc => doc.data());
+    var i = 0;
+    for(i = 0; i < courses.length; i++) { 
+        opt = document.createElement('option');
+        opt.value = courses[i].id;
+        opt.innerHTML = courses[i].name;
+        d.appendChild(opt);
+    }
+}
 
 export default ScheduleSearch
